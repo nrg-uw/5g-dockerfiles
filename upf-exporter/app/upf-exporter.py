9b6ecb014ca2972b4fc16c8c5d027c2a28d37747
Niloy Saha <niloysaha.ns@gmail.com>
2023-04-23 01:24:14 -0400

    Update exporter to v4.0

--- upf-exporter/app/upf-exporter.py
+++ upf-exporter/app/upf-exporter.py
@@ -1,15 +1,23 @@
-#!/usr/bin/python3
+#!/usr/local/bin/python
 import os
-import re
-import time
-import subprocess
+import json
 import logging
-from prometheus_client import start_http_server, Gauge, REGISTRY, PLATFORM_COLLECTOR, PROCESS_COLLECTOR, GC_COLLECTOR
+import re
+import subprocess
+import time
 
-logging.basicConfig(level=logging.INFO,
-                    format='%(asctime)s | %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
+from prometheus_client import start_http_server, Gauge, REGISTRY
+from prometheus_client import PLATFORM_COLLECTOR, PROCESS_COLLECTOR, GC_COLLECTOR
 
-PDR_INFO_LINE_MATCHER = re.compile(r"\[PDR No\.(\d+) SEID (\d+) Info\]")
+# Define constants
+DEFAULT_SERVER_PORT = 9000
+DEFAULT_SLEEP_INTERVAL = 10
+DEFAULT_GTP_IFNAME = 'upfgtp'
+
+# Read environment variables
+SERVER_PORT = int(os.environ.get('SERVER_PORT', DEFAULT_SERVER_PORT))
+SLEEP_INTERVAL = int(os.environ.get('SLEEP_INTERVAL', DEFAULT_SLEEP_INTERVAL))
+GTP_IFNAME = os.environ.get('GTP_IFNAME', DEFAULT_GTP_IFNAME)
 
 # Define Prometheus metrics
 PACKET_COUNT = Gauge('pdr_packet_count', 'Number of packets', [
@@ -17,87 +25,73 @@ PACKET_COUNT = Gauge('pdr_packet_count', 'Number of packets', [
 BYTE_COUNT = Gauge('pdr_byte_count', 'Number of bytes', [
                    'seid', 'pdrid', 'n3_ip', 'n4_ip', 'direction'])
 
-SERVER_PORT = int(os.environ.get('SERVER_PORT', '9000'))
-SLEEP_INTERVAL = int(os.environ.get('SLEEP_INTERVAL', '10'))
-
+# Unregister default Prometheus collectors
 REGISTRY.unregister(PROCESS_COLLECTOR)
 REGISTRY.unregister(PLATFORM_COLLECTOR)
 REGISTRY.unregister(GC_COLLECTOR)
 
 
-def get_active_pdrs():
-    """
-    Returns a list of active PDRs, each represented by a dictionary containing the PDR ID and SEID.
-    Extracts the information from the output of the 'gtp5g-tunnel list pdr' command using regex.
-    The list is sorted by the PDR ID in ascending order. Returns an empty list on error.
-    """
+def extract_hostname():
+    hostname = str(os.getenv('HOSTNAME'))
+    parts = hostname.split('-')
+    if len(parts) < 2:
+        return hostname
+    upf = parts[1].upper()
+    return upf
+
+
+logging.basicConfig(
+    level=logging.INFO,
+    format=f"%(asctime)s | {extract_hostname()} | %(levelname)s | %(message)s",
+    datefmt='%Y-%m-%d %H:%M:%S'
+)
+
+
+def set_prometheus_metrics(pdr_stats_list):
+    for pdr in pdr_stats_list:
+        ul_label = {'seid': pdr['seid'], 'pdrid': pdr['pdrid'],
+                    'n3_ip': pdr['n3_ip'], 'n4_ip': pdr['n4_ip'], 'direction': 'UL'}
+        dl_label = {'seid': pdr['seid'], 'pdrid': pdr['pdrid'],
+                    'n3_ip': pdr['n3_ip'], 'n4_ip': pdr['n4_ip'], 'direction': 'DL'}
+        PACKET_COUNT.labels(**ul_label).set(pdr['ul_packet_count'])
+        PACKET_COUNT.labels(**dl_label).set(pdr['dl_packet_count'])
+        BYTE_COUNT.labels(**ul_label).set(pdr['ul_byte_count'])
+        BYTE_COUNT.labels(**dl_label).set(pdr['dl_byte_count'])
+
+
+def get_pdr_stats():
+    command = ['./gogtp5g-tunnel', 'list', 'stats']
+    # Run the command and capture the output
+    output = subprocess.check_output(command)
+    return parse_stats(output)
+
+
+def parse_stats(output):
     try:
-        completed_proc = subprocess.run(
-            ["/app/libgtp5gnl/tools/gtp5g-tunnel", "list", "pdr"], capture_output=True, text=True)
-        stdout = completed_proc.stdout
-
-        active_pdrs = []
-
-        for match in PDR_INFO_LINE_MATCHER.finditer(stdout):
-            pdrid, seid = match.groups()
-            active_pdrs.append({'pdrid': int(pdrid), 'seid': int(seid)})
-
-        active_pdrs_sorted = sorted(active_pdrs, key=lambda d: d['pdrid'])
-        return active_pdrs_sorted
-
-    except subprocess.CalledProcessError as e:
-        logging.warning(f"Error in getting active PDRs: {e}")
+        stats = json.loads(output)
+    except (json.JSONDecodeError, TypeError):
+        logging.warning("Invalid PDR statistics format...")
         return []
 
+    if stats is None:
+        logging.warning("No PDR statistics found...")
+        return []
 
-def get_pdr_stats(seid, pdrid):
-    """
-    Returns a dictionary containing packet and byte counts (both UL and DL) for a given PDR.
-
-    Parameters:
-    seid (int): the SEID of the PDR
-    pdrid (int): the ID of the PDR
-
-    Returns:
-    dict: a dictionary containing packet and byte counts (both UL and DL) for the PDR
-    """
-    try:
-
-        with open('/proc/gtp5g/pdr', 'w') as f:
-            f.write(f'upfgtp {seid} {pdrid}\n')
-
-        # Get the PDR information from the proc interface
-        completed_proc = subprocess.run(
-            ["cat", "/proc/gtp5g/pdr"], capture_output=True, text=True, check=True)
-        stdout = completed_proc.stdout
-
-        # Parse the packet and byte counts from the output
-        ul_packet_count_match = re.search(r"UL Packet Count: (\d+)", stdout)
-        dl_packet_count_match = re.search(r"DL Packet Count: (\d+)", stdout)
-        ul_byte_count_match = re.search(r"UL Byte Count: (\d+)", stdout)
-        dl_byte_count_match = re.search(r"DL Byte Count: (\d+)", stdout)
-
-        # Create and return the dictionary of PDR statistics
+    pdr_stats_list = []
+    for s in stats:
         pdr_stats = {
-            'seid': seid,
-            'pdrid': pdrid,
-            'ul_packet_count': int(ul_packet_count_match.group(1)),
-            'dl_packet_count': int(dl_packet_count_match.group(1)),
-            'ul_byte_count': int(ul_byte_count_match.group(1)),
-            'dl_byte_count': int(dl_byte_count_match.group(1)),
+            'seid': s['SEID'],
+            'pdrid': s['ID'],
+            'ul_packet_count': s['UL_PKT_CNT'],
+            'dl_packet_count': s['DL_PKT_CNT'],
+            'ul_byte_count': s['UL_BYTE_CNT'],
+            'dl_byte_count': s['DL_BYTE_CNT'],
             'timestamp': int(time.time()),
             'n3_ip': get_upf_ip_addr('n3'),
             'n4_ip': get_upf_ip_addr('n4')
         }
-        return pdr_stats
-
-    except IOError as e:
-        logging.error(f"Error writing to proc file: {e}")
-
-    except subprocess.CalledProcessError as e:
-        logging.error(
-            f"Error in getting PDR stats for SEID {seid} and PDRID {pdrid}: {e}")
-        return {}
+        pdr_stats_list.append(pdr_stats)
+    return pdr_stats_list
 
 
 def get_upf_ip_addr(iface: str) -> str:
@@ -122,47 +116,26 @@ def get_upf_ip_addr(iface: str) -> str:
 
 def log_data(data):
     for log in data:
-        packet_counts = f"({log['ul_packet_count']},{log['dl_packet_count']})"
-        byte_counts = f"({log['ul_byte_count']},{log['dl_byte_count']})"
-        log_str = f"seid={log['seid']}|pdrid={log['pdrid']}|packet_counts={packet_counts}|byte_counts={byte_counts}|n3_ip={log['n3_ip']}|n4_ip={log['n4_ip']}"
+        packet_counts = f"({log['ul_packet_count']}, {log['dl_packet_count']})"
+        byte_counts = f"({log['ul_byte_count']}, {log['dl_byte_count']})"
+        seid, pdrid, n3_ip, n4_ip = log['seid'], log['pdrid'], log['n3_ip'], log['n4_ip']
+        log_str = f"seid={seid} | pdrid={pdrid} | packet_counts={packet_counts} | byte_counts={byte_counts} | n3_ip={n3_ip} | n4_ip={n4_ip}"
         logging.info(log_str)
-
-
-def get_active_pdrs_stats():
-    """
-    Returns a list of dictionaries containing the PDR statistics for all active PDRs.
-    Each dictionary contains 'pdrid', 'seid', 'ul_packet_count', 'dl_packet_count', 'ul_byte_count',
-    and 'dl_byte_count' keys.
-    """
-    active_pdrs = get_active_pdrs()
-    pdr_stats = []
-    for pdr in active_pdrs:
-        stats = get_pdr_stats(pdr['seid'], pdr['pdrid'])
-        pdr_stats.append(stats)
-    return pdr_stats
 
 
 def main():
     start_http_server(SERVER_PORT)
-    logging.info("Starting Prometheus server ...")
+    logging.info(f"Starting Prometheus server on port {SERVER_PORT}...")
     while True:
         try:
-            pdr_stats = get_active_pdrs_stats()
-            if not pdr_stats:
-                logging.warning(f"No PDR statistics found ...")
-            log_data(pdr_stats)
-            for pdr in pdr_stats:
-                ul_label = {'seid': pdr['seid'], 'pdrid': pdr['pdrid'],
-                            'n3_ip': pdr['n3_ip'], 'n4_ip': pdr['n4_ip'], 'direction': 'UL'}
-                dl_label = {'seid': pdr['seid'], 'pdrid': pdr['pdrid'],
-                            'n3_ip': pdr['n3_ip'], 'n4_ip': pdr['n4_ip'], 'direction': 'DL'}
-                PACKET_COUNT.labels(**ul_label).set(pdr['ul_packet_count'])
-                PACKET_COUNT.labels(**dl_label).set(pdr['dl_packet_count'])
-                BYTE_COUNT.labels(**ul_label).set(pdr['ul_byte_count'])
-                BYTE_COUNT.labels(**dl_label).set(pdr['dl_byte_count'])
-            time.sleep(SLEEP_INTERVAL)
+            pdr_stats = get_pdr_stats()
+            if pdr_stats:
+                log_data(pdr_stats)
+                set_prometheus_metrics(pdr_stats)
         except Exception as e:
-            logging.error(e)
+            logging.exception(e)
+
+        time.sleep(SLEEP_INTERVAL)
 
 
 if __name__ == "__main__":
